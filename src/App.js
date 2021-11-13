af0cc54765aa8a3d084596aaf002cecfb9265c3d
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-11-13 20:46:08 -0300

    add AddTransaction, Balance, IncomeExpenses, TransactionList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
+import AddTransaction from './components/AddTransaction';
 import Balance from './components/Balance';
 import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </>
   );
